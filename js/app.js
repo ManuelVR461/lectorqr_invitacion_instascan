5aa58c92ecd7218ff3fd6476163205c0d9b0e59e
ManuelVR461 <ManuelVR461@gmail.com>
2019-09-10 13:31:40 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+$(function(){
+  var scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+  scanner.addListener('scan', function (content) {
+      if(content){
+        var p = {content};
+        alert()
+        ValidarAcceso(p);
+      }
+  });
+
+  Instascan.Camera.getCameras().then(function (cameras) {
+    if (cameras.length > 0) {
+        scanner.start(cameras[0]);
+    } else {
+        console.error('No cameras found.');
+    }
+  }).catch(function (e) {
+      console.log(e);
+  });
+
+  const overlay = document.getElementById('overlay')
+  const getEmail = document.getElementById('getmail')
+  const modal = document.getElementById('modal')
+  const login__submit = document.getElementById('login__submit')
+          
+  getEmail.addEventListener('click', (event) => {
+      /*  $modal.style.animation = 'modalOut .8s forwards'; */
+      /*  $overlay.classList.remove('active'); */
+      overlay.classList.add('active');
+      modal.style.animation = 'modalIn 1.5s forwards';
+  });
+
+  function ValidarAcceso(p){
+      $.ajax({
+        async: true,
+        type: "POST",
+        data: p,
+        success: (data) => {
+          $("#datosleidos").empty().append(data);
+            if(data=="Invitado"){
+              alert('Acceso Permitido');
+            }else{
+              alert('No estas invitado');
+            }
+        },
+        url: './functions/validarinvitacion.php'
+    });
+  }
+});
+
+        
